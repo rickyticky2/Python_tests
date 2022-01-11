f4db0b44442b99443f454435ab8cfd06e699229a
azverev <aazwerew@gmail.com>
2022-01-11 22:27:46 +0700

    add ip check test

---
+++ ip.py
@@ -0,0 +1,17 @@
+# check ip
+
+import re
+ip = 0
+msg = ""
+
+msg = input("Enter IP: ")
+ip = str(msg)
+
+check = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+results = re.match(check, ip)
+
+if results != None:
+    print("IP address " + results.group() + " is OK")
+else:
+    print("IP address " + msg + " is invalid")
+
